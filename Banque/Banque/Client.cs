8f8c6cc88d6ea98a6bc89fb24b98dba3d2794da4
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-01-21 12:20:23 +0100

    oigyf
    iuh

--- Banque/Banque/Client.cs
+++ Banque/Banque/Client.cs
@@ -1,9 +1,17 @@
-﻿namespace Banque
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banque
 {
     class Client
     {
         private int id;
         private string nom;
+        private string prenom;
+        private string age;
 
         public string Nom
         {
@@ -11,6 +19,25 @@
             { return nom; }
             set
             { nom = value; }
+        }
+        public string Prenom
+        {
+            get
+            { return prenom; }
+            set
+            { prenom = value; }
+        }
+        public string Age
+        {
+            get
+            { return age; }
+            set
+            { age = value; }
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("nom: " + nom + ", prénom: " + prenom + ", Âge: " + age);
         }
     }
 }
--- Banque/Banque/Program.cs
+++ Banque/Banque/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,10 +11,16 @@ namespace Banque
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("a: " + 12 + ", b: " + 45);
+            Console.WriteLine(string.Format("a: {0:C}, b:{1}", 12, 46));
+
             Client dupont = new Client();
 
             dupont.Nom = "Dupont";
-            Console.WriteLine("nom = " + dupont.Nom);
+            dupont.Prenom = "Rudolph";
+            dupont.Age = "12ans";
+            dupont.Afficher();
         }
     }
 }
