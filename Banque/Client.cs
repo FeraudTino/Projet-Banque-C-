1dd2520dcd4cd508c6ba1822f36009099206be13
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-01-28 09:35:10 +0100

    Variable ajouté

--- Banque/Client.cs
+++ Banque/Client.cs
@@ -1,4 +1,11 @@
 ﻿class Client
 {
+    public int IdClient;
+    public char nom;
+    public char prenom;
+    public int DateNaissance;
+    public char adresse;
+    public int telephone;
+    public char mail;
 
 }
--- Banque/Compte.cs
+++ Banque/Compte.cs
@@ -1,4 +1,8 @@
 class Compte
 {
-
+    public int IdCompte;
+    public it libelle;
+    public char type;
+    public double solde;
+    public int decouvert;
 }
